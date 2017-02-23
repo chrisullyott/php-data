0b8bac54505111ee2712df3171ecc2f3835191cc
Chris Ullyott <chris@monkdevelopment.com>
2017-02-22 18:34:52 -0800

    Update URL methods for query strings

--- src/Url.php
+++ src/Url.php
@@ -29,10 +29,73 @@ class Url
 
     /**
      * Remove the query string from a URL.
+     *
+     * @param  string $url The URL including a query string
+     * @return string
      */
     public static function removeQueryString($url)
     {
-        return preg_replace('/(\/[^?]+).*/', '$1', $url);
+        return strtok($url, '?');
+    }
+
+    /**
+     * Remove a query parameter from a URL. The entire query string will be removed
+     * when there is only one parameter.
+     *
+     * @param  string $url The URL including a query string
+     * @param  string $key The parameter name
+     * @return string
+     */
+    public static function removeQueryParameter($url, $key)
+    {
+        parse_str(parse_url($url, PHP_URL_QUERY), $params);
+
+        unset($params[$key]);
+
+        if ($query = http_build_query($params)) {
+            return strtok($url, '?') . '?' . $query;
+        } else {
+            return strtok($url, '?');
+        }
+    }
+
+    /**
+     * Add or update a query parameter in a URL.
+     *
+     * @param string $url   The URL including a query string
+     * @param string $key   The parameter name
+     * @param string $value The new value for the parameter
+     * @return string
+     */
+    public static function addQueryParameter($url, $key, $value)
+    {
+        parse_str(parse_url($url, PHP_URL_QUERY), $params);
+
+        $params[$key] = $value;
+
+        return strtok($url, '?') . '?' . http_build_query($params);
+    }
+
+    /**
+     * Checks if a URL is absolute (begins with a protocol).
+     *
+     * @param  string $url URL to check for protocol existence
+     * @return bool true if the given string begins with a protocol
+     */
+    public static function isAbsoluteUrl($url)
+    {
+        return (bool) parse_url($url, PHP_URL_SCHEME);
+    }
+
+    /**
+     * Checks if a string is a relative URL.
+     *
+     * @param  string $url URL to check for relativity
+     * @return bool true if the given string begins with a forward slash
+     */
+    public static function isRelativeUrl($url)
+    {
+        return substr($url, 0, 1) == '/';
     }
 
     /**
