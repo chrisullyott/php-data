b43020604a504c3df9bd2d781be4314801a2af09
Chris Ullyott <chris@monkdevelopment.com>
2017-05-06 15:17:54 -0700

    Add formatByes method

--- src/File.php
+++ src/File.php
@@ -119,4 +119,17 @@ class File
         }
     }
 
+    public static function formatBytes($bytes, $precision = 0)
+    {
+        $units = array('B', 'KB', 'MB', 'GB', 'TB');
+
+        $bytes = max($bytes, 0);
+        $pow = floor(($bytes ? log($bytes) : 0) / log(1024));
+        $pow = min($pow, count($units) - 1);
+
+        $bytes /= pow(1024, $pow);
+
+        return round($bytes, $precision) . ' ' . $units[$pow];
+    }
+
 }
